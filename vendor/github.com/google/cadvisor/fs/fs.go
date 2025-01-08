d237af1a4198e34877d410771ebd9f70f29173e5
Pragash Vijayaragavan <pragash@robin.io>
2025-01-07 20:18:04 -0800

    add check for vfsstats

--- vendor/github.com/google/cadvisor/fs/fs.go
+++ vendor/github.com/google/cadvisor/fs/fs.go
@@ -424,6 +424,10 @@ func (i *RealFsInfo) GetFsInfoForPath(mountSet map[string]struct{}) ([]Fs, error
 				if robinfs.FileSystemHung(partition.mountpoint, 3) {
 					err = fmt.Errorf("File system hung.")
 				}
+				if err != nil {
+					klog.V(4).Infof("the file system is hung")
+					break
+				}
 				var inodes, inodesFree uint64
 				fs.Capacity, fs.Free, fs.Available, inodes, inodesFree, err = getVfsStats(partition.mountpoint)
 				if err != nil {
